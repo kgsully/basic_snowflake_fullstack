a0a9569296bccc9ed8043b0bd752bae22c88eb09
Ken Sully <ken.sully@gmail.com>
2025-03-21 16:21:24 -0600

    router cleanup

--- frontend_angular/src/app/app.component.ts
+++ frontend_angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink, RouterOutlet} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +7,6 @@ import { Router, RouterLink, RouterOutlet} from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.router.navigate(['houses']);
-  }
+export class AppComponent {
 
 }
